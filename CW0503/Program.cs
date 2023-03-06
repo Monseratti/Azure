d0be4c8a3de454b2f284de05403a87a2ca9e3241
Monseratti <dnamaxy@gmail.com>
2023-03-06 10:57:42 +0200

    commit

---
+++ CW0503/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Blobs;
+
+var conn = "DefaultEndpointsProtocol=https;AccountName=monseratti;AccountKey=7Wmx4jeBwJsEXYPMwDUddz/BuiGsqxwkdZTjl6VkGPavHXELcZ0lg+iIor8pR/Q8W/BWWOkPUHY6+AStwazEUg==;EndpointSuffix=core.windows.net";
+BlobServiceClient bsc = new BlobServiceClient(conn);
+
+var containerName = $"container{Guid.NewGuid()}";
+var localPath = "data";
+Directory.CreateDirectory(localPath);
+var fileName = "test.json";
+
+await File.WriteAllTextAsync(Path.Combine(localPath, fileName), 
+	"{\"name\":\"Hello\"}");
+
+BlobContainerClient bcc = await bsc.CreateBlobContainerAsync(containerName);
+
+BlobClient bc = bcc.GetBlobClient(fileName);
+
+await bc.UploadAsync(Path.Combine(localPath, fileName));
+
+await foreach(var file in bcc.GetBlobsAsync())
+{
+    Console.WriteLine(file.Name);
+}
---
+++ HW0503/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Blobs;
+
+var conn = "DefaultEndpointsProtocol=https;AccountName=monseratti;AccountKey=7Wmx4jeBwJsEXYPMwDUddz/BuiGsqxwkdZTjl6VkGPavHXELcZ0lg+iIor8pR/Q8W/BWWOkPUHY6+AStwazEUg==;EndpointSuffix=core.windows.net";
+
+var localPath = "savedata";
+Directory.CreateDirectory(localPath);
+
+BlobContainerClient containerClient = new BlobContainerClient(conn, "containerac4c92d0-0d36-4876-91e1-006fbac00f3c");
+
+BlobClient blobClient = containerClient.GetBlobClient("test.json");
+await blobClient.DownloadToAsync(localPath);
+
+Console.WriteLine(File.ReadAllText($"{localPath}\\test.json"));
+Console.ReadLine();
